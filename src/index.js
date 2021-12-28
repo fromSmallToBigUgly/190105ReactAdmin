148803fd8bdb3a28b5ce0690a8804ef1bbecfc6e
于占玖 <1135567019@qq.com>
2021-12-28 17:04:40 +0900

    init app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+入口文件
+* */
+import React from "react";
+import ReactDOM from "react-dom"
+//import 'antd/dist/antd.css'
+
+import App from './App'
+//将App标签渲染到index的div上面
+ReactDOM.render(<App/>,document.getElementById('root'))
